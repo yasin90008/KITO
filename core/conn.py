1ae9e2a717b26c117b2970617838e45ec7a9c834
Honey Pots <54258853+HoneyPots0@users.noreply.github.com>
2020-11-13 17:32:43 +0000

    Add files via upload

---
+++ core/conn.py
@@ -0,0 +1,52 @@
+import os
+import netifaces
+import random
+import time
+import subprocess
+
+Red = '\033[1;31m'
+Blue= '\033[1;36m'
+Endc = '\033[0m'
+
+line = "----------------------------------------------------------"
+
+def clr():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+def main():
+    clr()
+    logo="""
+ __   __  ___   __          ___      __       ___      ___ 
+|"  |/  \|  "| |" \        |"  |    /""\     |"  \    /"  |
+|'  /    \:  | ||  |       ||  |   /    \     \   \  //   |
+|: /'        | |:  |       |:  |  /' /\  \    /\\  \/.    |
+ \//  /\'    | |.  |    ___|  /  //  __'  \  |: \.        |
+ /   /  \\   | /\  |\  /  :|_/ )/   /  \\  \ |.  \    /:  |
+|___/    \___|(__\_|_)(_______/(___/    \___)|___|\__/|___|
+
+   ""","""
+----------------------       ------------------------------
+|      SecAnon       |       |     Version :  1.0Beta     |
+----------------------       ------------------------------
+
+\t        Created by Honey Pots...
+
+---------------------------------------------------------- 
+"""
+    print(Red+logo[0]+Blue+logo[1])
+
+main()
+import sys
+print(Blue)
+list_argv = list(sys.argv)
+nm = list_argv[1]+'mon'
+command = 'airodump-ng ' + nm 
+os.system(command)
+
+input('Please Enter To Exit : ')
+def end():
+    foo=raw_input()
+    sys.exit()
+end()
